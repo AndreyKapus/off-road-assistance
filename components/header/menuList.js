33c3ba9b32d2ed784f5b6f1f1746e459c1da87a1
AndreyKapus <kapustnikov@ukr.net>
2024-02-04 12:52:28 +0200

    menu button

--- components/header/menuList.js
+++ components/header/menuList.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import styles from "../../app/styles/header.module.css";
+import { IoMenu } from "react-icons/io5";
 
 const MenuList = () => {
   return (
     <>
+      <button type="button">
+        <IoMenu />
+      </button>
       <ul className={styles.linksList}>
         <li>
           <Link href="/contacts">Контакти</Link>
