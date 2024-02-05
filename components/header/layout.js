bb8452024b13989d9d43e2dcd6bed75f84aae0c3
AndreyKapus <kapustnikov@ukr.net>
2024-02-05 15:48:34 +0200

    mobile menu

--- components/header/layout.js
+++ components/header/layout.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "../../app/styles/header.module.css";
 import MenuList from "./menuList";
 import Logo from "./logo";
+import OpenMobileMenuBtn from "./openMobileMenuBtn";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <Link href="/" className={styles.logoLink}>
         <Logo />
       </Link>
+      <OpenMobileMenuBtn />
       <MenuList />
     </header>
   );
---
+++ components/header/openMobileMenuBtn.js
@@ -0,0 +1,14 @@
+import { IoMdMenu } from "react-icons/io";
+import styles from "../../app/styles/header.module.css";
+
+const OpenMobileMenuBtn = () => {
+  return (
+    <>
+      <button type="button" className={styles.mobileMenuBtn}>
+        <IoMdMenu size={24} />
+      </button>
+    </>
+  );
+};
+
+export default OpenMobileMenuBtn;
