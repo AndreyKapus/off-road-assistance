02a06adff3faa8b3bb0a5e32334229465a5b1c59
AndreyKapus <kapustnikov@ukr.net>
2024-02-05 16:27:16 +0200

    menu

--- components/header/layout.js
+++ components/header/layout.js
@@ -3,15 +3,22 @@ import styles from "../../app/styles/header.module.css";
 import MenuList from "./menuList";
 import Logo from "./logo";
 import OpenMobileMenuBtn from "./openMobileMenuBtn";
+// import { useEffect, useState } from "react";
 
 const Layout = () => {
+  // const [screenWidth, setScreenWidth] = useState(null);
+
+  // useEffect(() => {
+  //   const width = window.screen.width;
+  //   setScreenWidth(width);
+  // }, []);
+
   return (
     <header className={styles.headerWrapper}>
       <Link href="/" className={styles.logoLink}>
         <Logo />
       </Link>
-      <OpenMobileMenuBtn />
-      <MenuList />
+      {/* {screenWidth < 768 ? <OpenMobileMenuBtn /> : <MenuList />} */}
     </header>
   );
 };
---
+++ components/header/mobileMenu.js
@@ -0,0 +1,9 @@
+const { default: MenuList } = require("./menuList");
+
+const MobileMenu = () => {
+  return (
+    <>
+      <MenuList />
+    </>
+  );
+};
