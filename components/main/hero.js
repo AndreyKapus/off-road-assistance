cbd5c9be0806beb6b4a859b8eb131a7773397f62
AndreyKapus <kapustnikov@ukr.net>
2024-02-14 12:19:26 +0200

    hero desktop

--- components/main/hero.js
+++ components/main/hero.js
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
     <div className={styles.heroTitleWrapper}>
       <h1 className={styles.heroTitle}>Roadside Assistance</h1>
-      <button className={styles.heroCallBtn}>Викликати</button>
+      <button className={styles.heroCallBtn}>
+        <a href="tel:+380961111111" class="tel__link">
+          Викликати
+        </a>
+      </button>
     </div>
   );
 };
