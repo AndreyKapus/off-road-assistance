c8cbbea6119e52fd5a8b05ee8f2f1b5c392ac54c
AndreyKapus <kapustnikov@ukr.net>
2024-02-21 13:04:32 +0200

    services

--- app/main/main.js
+++ app/main/main.js
@@ -1,12 +1,14 @@
 import Hero from "@/components/main/hero";
 import styles from "../styles/hero.module.css";
 import About from "@/components/main/about";
+import ServicesMain from "@/components/main/servises";
 
 const MainPage = () => {
   return (
     <>
       <Hero />
       <About />
+      <ServicesMain />
     </>
   );
 };
---
+++ components/main/servises.js
@@ -0,0 +1,13 @@
+import styles from "../../app/styles/servicesMain.module.css";
+
+const ServicesMain = () => {
+  return (
+    <section className={styles.servicesSection}>
+      <div className={styles.servicesContainer}>
+        <h3>Наші сервіси</h3>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesMain;
