c81b7bae00de3f292ae1b4ed2e558ccef9c1d030
AndreyKapus <kapustnikov@ukr.net>
2024-03-02 11:19:05 +0200

    servises content

--- components/main/servises.js
+++ components/main/servises.js
@@ -4,8 +4,17 @@ const ServicesMain = () => {
   return (
     <section className={styles.servicesSection}>
       <div className={styles.servicesContainer}>
-        <h3 className={styles.servicesTitle}>Наші сисерві</h3>
+        <h3 className={styles.servicesTitle}>Наші сервіси</h3>
       </div>
+      <ul>
+        <li>
+          <img src="#" alt="car stuck in the mud" />
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 };
