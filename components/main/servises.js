9dff1758c6bf7fac73caae468816e8879c6d6b25
AndreyKapus <kapustnikov@ukr.net>
2024-03-07 18:24:25 +0200

    services item

--- components/main/servises.js
+++ components/main/servises.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import CarStuck from "../../images/stuck-in-mud.jpg";
 import styles from "../../app/styles/servicesMain.module.css";
 
 const ServicesMain = () => {
@@ -7,21 +9,27 @@ const ServicesMain = () => {
         <h3 className={styles.servicesTitle}>Наші сервіси</h3>
       </div>
       <ul>
-        <li>
-          <img src="#" alt="car stuck in the mud" />
+        <li className={styles.servicesListItem}>
+          <Image
+            src={CarStuck}
+            width={300}
+            height={200}
+            alt="car stuck in the mud"
+          />
+
           <div>
             <h3>Витяжка авто</h3>
-            <p></p>
+            <p>Витягнемо з будь-якої залупи!</p>
           </div>
         </li>
-        <li>
+        <li className={styles.servicesListItem}>
           <img src="#" alt="car stuck in the mud" />
           <div>
             <h3>Аварійне відкриття замків</h3>
             <p></p>
           </div>
         </li>
-        <li>
+        <li className={styles.servicesListItem}>
           <img src="#" alt="car stuck in the mud" />
           <div>
             <h3>Відкриття домашніх замків</h3>
