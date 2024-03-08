4d607d128970ad8d91d556b8c771d86186e6ab09
AndreyKapus <kapustnikov@ukr.net>
2024-03-08 13:06:22 +0200

    padding

--- components/main/servises.js
+++ components/main/servises.js
@@ -19,7 +19,13 @@ const ServicesMain = () => {
             />
             <div className={styles.servisesContentWrapper}>
               <h3 className={styles.servisesListTitle}>Витяжка авто</h3>
-              <p>Витягнемо з будь-якої залупи!</p>
+              <p>
+                Витягнемо з будь-якої залупи! Compressing objects: 100% (5/5),
+                done. Writing objects: 100% (5/5), 621 bytes | 621.00 KiB/s,
+                done. Total 5 (delta 4), reused 0 (delta 0), pack-reused 0
+                remote: Resolving deltas: 100% (4/4), completed with 4 local
+                objects.
+              </p>
             </div>
           </li>
           <li className={styles.servicesListItem}>
