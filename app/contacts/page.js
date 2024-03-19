835daf32e2ee8bca520808799a9644c2c9eb490c
AndreyKapus <kapustnikov@ukr.net>
2024-03-19 12:08:15 +0200

    contacts section

---
+++ app/contacts/page.js
@@ -0,0 +1,13 @@
+import styles from "../../app/styles/contacts.module.css";
+
+const Contacts = () => {
+  return (
+    <section className={styles.contactsSection}>
+      <div className={styles.contactsContainer}>
+        <h2>Контакты</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Contacts;
