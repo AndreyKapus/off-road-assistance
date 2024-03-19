d66d9783c73dc904c8c04fb6b2311df1ddb30955
AndreyKapus <kapustnikov@ukr.net>
2024-03-19 12:31:19 +0200

    contacts

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -5,6 +5,17 @@ const Contacts = () => {
     <section className={styles.contactsSection}>
       <div className={styles.contactsContainer}>
         <h2>Контакты</h2>
+        <ul>
+          <li>
+            Телефон: <a href="tel: 0000000000">0000000000</a>
+          </li>
+          <li>
+            Viber: <a href="tel: 0000000000">0000000000</a>
+          </li>
+          <li>
+            Telegram: <a href="tel: 0000000000">0000000000</a>
+          </li>
+        </ul>
       </div>
     </section>
   );
