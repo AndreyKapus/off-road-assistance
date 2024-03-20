6ae8f90cd21fb223c5517b5ff51da3762bec0ad7
AndreyKapus <kapustnikov@ukr.net>
2024-03-20 12:18:26 +0200

    rates

--- app/rates/page.js
+++ app/rates/page.js
@@ -3,7 +3,23 @@ import styles from "../../app/styles/rates.module.css";
 const Rates = () => {
   return (
     <section className={styles.ratesSection}>
-      <div className={styles.ratesContainer}>Тарифи</div>
+      <div className={styles.ratesContainer}>
+        <h3>Тарифи</h3>
+        <ul className={styles.ratesList}>
+          <li className={styles.ratesListItem}>
+            Витяжка авто із бруду/снігу:
+            <span className={styles.ratesListRate}>від 300 грн</span>
+          </li>
+          <li className={styles.ratesListItem}>
+            Аварійне відкриття автомобільних дверей:
+            <span className={styles.ratesListRate}>від 300 грн</span>
+          </li>
+          <li className={styles.ratesListItem}>
+            Аварійне відкриття квартири:
+            <span className={styles.ratesListRate}>від 300 грн</span>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
