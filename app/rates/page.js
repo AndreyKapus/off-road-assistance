04e6754ff16cfb9cf488e140b818c449f4ac854d
AndreyKapus <kapustnikov@ukr.net>
2024-03-23 12:40:40 +0200

    fix

--- app/rates/page.js
+++ app/rates/page.js
@@ -18,10 +18,6 @@ const Rates = () => {
             Аварійне відкриття квартири:
             <span className={styles.ratesListRate}>від 300 грн</span>
           </li>
-          <li className={styles.ratesListItem}>
-            Аварійне відкриття квартири:
-            <span className={styles.ratesListRate}>від 300 грн</span>
-          </li>
         </ul>
       </div>
     </section>
