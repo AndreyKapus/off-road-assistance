1ddff0403a462ce0c3da91e5d0a24e6d613c97f4
AndreyKapus <kapustnikov@ukr.net>
2024-03-26 13:19:44 +0200

    footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,9 +1,20 @@
 import styles from "../../app/styles/footer.module.css";
+import Link from "next/link";
+import Logo from "../header/logo";
 
 const Footer = () => {
   return (
     <section className={styles.footerSection}>
-      <div className={styles.footerContainer}></div>
+      <div className={styles.footerContainer}>
+        <Link href="/" className={styles.logoLink}>
+          <Logo />
+        </Link>
+        <ul>
+          <li>Телефон</li>
+          <li>Viber</li>
+          <li>Telegram</li>
+        </ul>
+      </div>
     </section>
   );
 };
