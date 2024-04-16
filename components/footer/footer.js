da663e409e99d89ced502cf655f0ca1eec594c69
AndreyKapus <kapustnikov@ukr.net>
2024-04-16 20:23:04 +0300

    fix

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -34,12 +34,6 @@ const Footer = () => {
               assistans@mail.com
             </a>
           </li>
-          <li className={styles.contactsItem}>
-            <p className={styles.contactsTitle}>Email:</p>
-            <a href="mailto:assistans@mail.com" className={styles.contactInfo}>
-              assistans@mail.com
-            </a>
-          </li>
         </ul>
       </div>
     </section>
