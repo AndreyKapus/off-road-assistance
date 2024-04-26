a506a2db5bae415b3386e34c3cc38a86ce699551
AndreyKapus <kapustnikov@ukr.net>
2024-04-26 17:31:01 +0300

    fiX

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -34,6 +34,12 @@ const Footer = () => {
               assistans@mail.com
             </a>
           </li>
+          <li className={styles.contactsItem}>
+            <p className={styles.contactsTitle}>instagram:</p>
+            <a href="mailto:assistans@mail.com" className={styles.contactInfo}>
+              assistans@mail.com
+            </a>
+          </li>
         </ul>
       </div>
     </section>
