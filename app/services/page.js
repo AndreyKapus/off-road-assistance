14412913162e358bec2953d0c4d56b3136311a8a
AndreyKapus <kapustnikov@ukr.net>
2024-05-11 13:02:01 +0300

    services page

---
+++ app/services/page.js
@@ -0,0 +1,11 @@
+import styles from "../styles/services.module.css";
+
+const Services = () => {
+  return (
+    <section className={styles.servicesSection}>
+      <h2>Наші сервіси</h2>
+    </section>
+  );
+};
+
+export default Services;
