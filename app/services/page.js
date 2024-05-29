e7b7ce9d1a13f7fc07f1753fa065dc98c31c5fdf
AndreyKapus <kapustnikov@ukr.net>
2024-05-29 15:06:08 +0300

    servicec

--- app/services/page.js
+++ app/services/page.js
@@ -4,6 +4,7 @@ const Services = () => {
   return (
     <section className={styles.servicesSection}>
       <h2 className="servisesTitle">Наші сервіси</h2>
+      <div className="servicesWrapper"></div>
     </section>
   );
 };
