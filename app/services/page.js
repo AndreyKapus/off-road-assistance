1885d3a44d654a0b3bca0b5d5cb7f8a9f63e284a
AndreyKapus <kapustnikov@ukr.net>
2024-06-09 16:03:20 +0300

    fix

--- app/services/page.js
+++ app/services/page.js
@@ -14,6 +14,11 @@ const Services = () => {
         <p>Text</p>
         <p>Image</p>
       </div>
+      <div className="servicesWrapper">
+        <h2>Title</h2>
+        <p>Text</p>
+        <p>Image</p>
+      </div>
     </section>
   );
 };
