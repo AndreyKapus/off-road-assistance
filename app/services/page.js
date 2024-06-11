b7e682dc3b289e8138cee49dd62b816909b7edb4
AndreyKapus <kapustnikov@ukr.net>
2024-06-11 11:45:23 +0300

    services

--- app/services/page.js
+++ app/services/page.js
@@ -1,4 +1,6 @@
 import styles from "../styles/services.module.css";
+import Image from "next/image";
+import carStuckInSnow from "../../images/stuck_in_snow.jpg";
 
 const Services = () => {
   return (
@@ -6,9 +8,13 @@ const Services = () => {
       <div className={styles.servicesContainer}>
         <h2 className="servisesTitle">Наші сервіси</h2>
         <div className="servicesWrapper">
-          <h2>Title</h2>
-          <p>Text</p>
-          <p>Image</p>
+          <Image
+            src={carStuckInSnow}
+            alt="car stuck in snow"
+            className={styles.servicesImage}
+          />
+          <h2>Витягування машини</h2>
+          <p>Витягнемо авто із снігу, болота або піску</p>
         </div>
         <div className="servicesWrapper">
           <h2>Title</h2>
