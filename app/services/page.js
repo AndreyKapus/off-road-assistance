4790bc7d57d098249335d0ee645a139be321719e
AndreyKapus <kapustnikov@ukr.net>
2024-06-11 11:30:59 +0300

    services

--- app/services/page.js
+++ app/services/page.js
@@ -3,16 +3,18 @@ import styles from "../styles/services.module.css";
 const Services = () => {
   return (
     <section className={styles.servicesSection}>
-      <h2 className="servisesTitle">Наші сервіси</h2>
-      <div className="servicesWrapper">
-        <h2>Title</h2>
-        <p>Text</p>
-        <p>Image</p>
-      </div>
-      <div className="servicesWrapper">
-        <h2>Title</h2>
-        <p>Text</p>
-        <p>Image</p>
+      <div className={styles.servicesContainer}>
+        <h2 className="servisesTitle">Наші сервіси</h2>
+        <div className="servicesWrapper">
+          <h2>Title</h2>
+          <p>Text</p>
+          <p>Image</p>
+        </div>
+        <div className="servicesWrapper">
+          <h2>Title</h2>
+          <p>Text</p>
+          <p>Image</p>
+        </div>
       </div>
     </section>
   );
